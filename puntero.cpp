9bd592cd2ba5835b34b8cd8db40655cf088b1bee
a13xanderplatz <a13xanderplatz10@outlook.com.ar>
2025-06-28 01:10:47 -0500

    Update puntero.cpp

--- puntero.cpp
+++ puntero.cpp
@@ -2,15 +2,15 @@
 #include <algorithm>
 using namespace std;
 
-// Functor para orden ascendente
-struct ascendente {
+class ascendente {
+public:
     bool operator()(const int& a, const int& b) const {
         return a < b;
     }
 };
 
-// Functor para orden descendente
-struct descendente {
+class descendente {
+public:
     bool operator()(const int& a, const int& b) const {
         return a > b;
     }
@@ -21,7 +21,15 @@ class ordena {
 public:
     void ordenar(T* inicio, T* fin) {
         O orden;
-        std::sort(inicio, fin, orden);/*hibrido(introsort) la stl decide internamente*/
+        for (T* i = inicio; i != fin; ++i) {
+            for (T* j = inicio; j < fin - 1; ++j) {
+                if (orden(*j, *(j + 1))) {
+                    T tmp = *j;
+                    *j = *(j + 1);
+                    *(j + 1) = tmp;
+                }
+            }
+        }
         cout << "Arreglo ordenado: ";
         for (T* it = inicio; it != fin; ++it) {
             cout << *it << " ";
@@ -29,6 +37,21 @@ public:
         cout << endl;
     }
 };
+
+//template <class T, class O>
+//class ordena {
+//public:
+    //void ordenar(T* inicio, T* fin) {
+        //O orden;
+       // std::sort(inicio, fin, orden);/*hibrido(introsort) la stl decide internamente*/
+        //cout << "Arreglo ordenado: ";
+       // for (T* it = inicio; it != fin; ++it) {
+       //     cout << *it << " ";
+       // }
+       // cout << endl;
+  //  }
+//};
+
 
 int main() {
     int arr[] = {9, 3, 13, 1, 5, 6};
